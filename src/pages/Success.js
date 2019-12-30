68443ff439cac44d37be01aafa3fbf92ee52016b
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-12-30 14:42:15 +0600

    hope it will be work

---
+++ src/pages/Success.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Success = () => {
+  return (
+    <div>
+      Form Send
+      <Link to="/">Go to website</Link>
+    </div>
+  );
+};
+
+export default Success;
