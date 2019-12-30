8c9e775a6b323941004322dcde99a03a5bc0672d
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-12-30 16:46:35 +0600

    contact demo make functional component

--- src/pages/Contactdemo.js
+++ src/pages/Contactdemo.js
@@ -1,35 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import NetlifyForm from "react-netlify-form";
 
-import NetlifyForm from 'react-netlify-form'
-
-
-class Contactdemo extends Component {
-  render() {
-    return (
-      <NetlifyForm name='Contact Form'>
+const Contactdemo = () => {
+  return (
+    <NetlifyForm name="Contact Form">
       {({ loading, error, success }) => (
         <div>
-          {loading &&
-            <div>Loading...</div>
-          }
-          {error &&
+          {loading && <div>Loading...</div>}
+          {error && (
             <div>Your information was not sent. Please try again later.</div>
-          }
-          {success &&
-            <div>Thank you for contacting us!</div>
-          }
-          {!loading && !success &&
+          )}
+          {success && <div>Thank you for contacting us!</div>}
+          {!loading && !success && (
             <div>
-              <input type='text' name='Name' required />
-              <textarea name='Message' required />
+              <input type="text" name="Name" required />
+              <textarea name="Message" required />
               <button>Submit</button>
             </div>
-          }
+          )}
         </div>
       )}
     </NetlifyForm>
-    );
-  }
-}
+  );
+};
 
 export default Contactdemo;
