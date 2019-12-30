c4d1f2fd54dfe087772e5833fea4ffbc825a1716
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-12-30 14:45:49 +0600

    edit form

--- src/pages/Contactdemo.js
+++ src/pages/Contactdemo.js
@@ -11,24 +11,22 @@ class Contactdemo extends Component {
         data-netlify-honeypot="bot-field"
       >
         <input type="hidden" name="bot-field" />
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" rows="6" required />
+        </div>
+        <div>
+          <input type="submit" value="Drop a line" />
+          <input type="reset" value="Eraser" />
+        </div>
       </form>
     );
   }
