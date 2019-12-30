d6befeda0cc6cd3633977f9e9044e459d0766285
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-12-30 14:36:10 +0600

    stateless form

--- src/pages/Contactdemo.js
+++ src/pages/Contactdemo.js
@@ -1,51 +1,19 @@
 import React, { Component } from 'react'
 
-
-const encode = (data) => {
-  return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-}
-
 class Contactdemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "", email: "", message: "" };
-  }
 
-  /* Here’s the juicy bit for posting the form submission */
-
-  handleSubmit = e => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
-    })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-
-    e.preventDefault();
-  };
-
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
   render() {
-    const { name, email, message } = this.state;
+
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form name="contact" method="post">
       <p>
-        <label>
-          Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-        </label>
+        <label>Your Name: <input type="text" name="name"/></label>
       </p>
       <p>
-        <label>
-          Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-        </label>
+        <label>Your Email: <input type="email" name="email"/></label>
       </p>
       <p>
-        <label>
-          Message: <textarea name="message" value={message} onChange={this.handleChange} />
-        </label>
+        <label>Message: <textarea name="message"></textarea></label>
       </p>
       <p>
         <button type="submit">Send</button>
