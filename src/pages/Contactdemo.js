23f2cd62a7638baeef59226d538758d88bebd28b
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-12-30 14:31:06 +0600

    contact demo try

--- src/pages/Contactdemo.js
+++ src/pages/Contactdemo.js
@@ -1,39 +1,58 @@
-import React from "react";
+import React, { Component } from 'react'
 
-const Contactdemo = () => {
-  return (
-    <div>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" defaultValue={""} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </div>
-  );
-};
 
-export default Contactdemo;
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
+class Contactdemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" value={message} onChange={this.handleChange} />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+    )
+  }
+}
+
+export default Contactdemo
