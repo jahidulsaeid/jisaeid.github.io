f20d06b5f7edd21ad838f04fa15e460f2590fe61
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-12-30 14:39:36 +0600

    contact form hidden type add

--- src/pages/Contactdemo.js
+++ src/pages/Contactdemo.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Contactdemo extends Component {
-
   render() {
-
     return (
-      <form name="contact" method="post">
-      <p>
-        <label>Your Name: <input type="text" name="name"/></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email"/></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-    )
+      <form
+        name="contact"
+        method="post"
+        action="/success"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="bot-field" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    );
   }
 }
 
-export default Contactdemo
+export default Contactdemo;
